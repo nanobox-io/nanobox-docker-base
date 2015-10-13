4bf554dd9155626623e525b35ce30e56df638703
Daniel Barney <daniel@pagodabox.com>
2015-10-13 16:14:23 -0600

    udpated test files

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -1,3 +1,4 @@
+UUID=$(cat /proc/sys/kernel/random/uuid)
 pass "unable to start thecontainer" docker run --privileged=true -d --name $UUID nanobox/nanobox-docker-postgresql:$VERSION
 defer docker kill $UUID
 
