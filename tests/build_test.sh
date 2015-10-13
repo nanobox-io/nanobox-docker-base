2d6bce29ecb6a85cae2e5379b73c82917f532774
Daniel Barney <daniel@pagodabox.com>
2015-10-13 16:17:44 -0600

    we are running the base image

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -1,5 +1,5 @@
 UUID=$(cat /proc/sys/kernel/random/uuid)
-pass "unable to start thecontainer" docker run --privileged=true -d --name $UUID nanobox/nanobox-docker-postgresql:$VERSION
+pass "unable to start thecontainer" docker run --privileged=true -d --name $UUID nanobox/base
 defer docker kill $UUID
 
 # now we just need to verify that the image is setup correctly
