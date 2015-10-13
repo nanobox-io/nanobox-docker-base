28826806f7b4270ec8747da8a2dab79a27204254
Daniel Barney <daniel@pagodabox.com>
2015-10-13 14:55:05 -0600

    added default test

---
+++ tests/build_test.sh
@@ -0,0 +1,4 @@
+pass "unable to start thecontainer" docker run --privileged=true -d --name $UUID nanobox/nanobox-docker-postgresql:$VERSION
+defer docker kill $UUID
+
+# now we just need to verify that the image is setup correctly
