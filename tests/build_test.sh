f0196002770aa9985d4731c1e86c8a1b97c2ca2f
Daniel Barney <daniel@pagodabox.com>
2015-10-14 15:23:01 -0600

    fixing command to run in the docker conatiner

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -4,4 +4,4 @@ defer docker kill $UUID
 
 # now we just need to verify that the image is setup correctly
 
-pass [ `ls -lh /home/gonano | head -n 2 | tail -n 1| awk '{print $3}'` == "gonano"]
+pass "home directory needs to be owned by gonano" docker exec %UUID bash -c "[ `ls -lh /home/gonano | head -n 2 | tail -n 1| awk '{print $3}'` == \"gonano\" ]"
