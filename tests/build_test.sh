3f03432085b6913c05c4d4691c1b8be9acc92b9c
Braxton Huggins <braxton@pagodabox.com>
2017-08-01 13:36:21 -0600

    modify the docker file to trigger building of nanobox/runit docker image

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -1,5 +1,6 @@
 UUID=$(cat /proc/sys/kernel/random/uuid)
 pass "unable to start the container" docker run -d --name $UUID nanobox/base
-defer docker kill $UUID
+defer docker stop $UUID
+defer docker rm $UUID
 
 # now we just need to verify that the image is setup correctly
